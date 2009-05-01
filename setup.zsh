5128f8fda4e840c3641b75e8b210ac3454166a36
javascripter <javascripter_@livedoor.com>
2009-05-02 03:37:16 +0900

    deleted profile

--- setup.zsh
+++ setup.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-files="zshrc zshenv vimrc gvimrc screenrc profile"
+files="zshrc zshenv vimrc gvimrc screenrc"
 for filename in ${(z)files}; do
   echo "create symlink from" "$PWD/$filename" "to" "$HOME/.$filename"
   ln -s "$PWD/$filename" "$HOME/.$filename"
--- zshenv
+++ zshenv
@@ -1,4 +1,6 @@
-source ~/.profile
+if [ -e ~/.profile ]; then
+  source ~/.profile
+fi
 export EDITOR=vi
 export LANG=ja_JP.UTF-8
 export PATH=/usr/local/bin/:$PATH
