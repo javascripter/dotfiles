ef22ddc6915b7fb465f031dfa92d1ebc1727fdf7
javascripter <javascripter@javascripter-no-macbook.local>
2009-05-02 02:16:46 +0900

    added setup.zsh

---
+++ setup.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+files="zshrc zshenv vimrc gvimrc screenrc profile"
+for filename in ${(z)files}; do
+  echo "create symlink from" "$PWD/$filename" "to" "$HOME/.$filename"
+  ln -s "$PWD/$filename" "$HOME/.$filename"
+done
